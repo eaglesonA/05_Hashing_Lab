92b4348c66a2161f28806980595df0dc62b30b38
unknown <eaglesa@EAS-BEN016T4.it.muohio.edu>
2014-10-07 16:37:17 -0400

    Previous work in class + add function

--- HashTable.h
+++ HashTable.h
@@ -2,14 +2,14 @@
 //Author: Bo Brinkman
 //Date: 2013/07/24
 #include "USet.h"
-
+//use hashprimes array constants 
 /*
  * Note: Just above your template declaration when you use this class, you
  * must define method called "hash" that takes a Key as input, and returns
  * an unsigned long (which is the hash value)
  *
  * For example, you might do:
- * unsigned long hash(char c){ return 10*((unsigned long)c)%13; }
+ * unsigned long hash(char c){ return 10*((unsigned long)c)%backingArraySize; }
  * HashTable<char,int> mySillyTable;
  *
  * If you don't define an appropriate hash function, the class won't compile.
@@ -69,7 +69,7 @@ private:
   unsigned long calcIndex(Key k);
 
   unsigned long numItems; //Number of items in the hash table
-
+  unsigned long primeNum;
   //Note: Ordinarily, these OUGHT to be private. In this case I have
   // made them public for easy of testing.
  public:
@@ -77,4 +77,94 @@ private:
   unsigned long backingArraySize;
 };
 
-#include "HashTable.ipp"
+
+
+//You will need this so you can make a string to throw in
+// remove
+#include <string>
+
+template <class Key, class T>
+HashTable<Key,T>::HashTable(){
+  numItems=0;
+  numRemoved=0;
+  primeNum=0;
+  backingArraySize= hashPrimes[primeNum];
+  backingArray = new HashRecord[backingArraySize];
+
+
+}
+
+template <class Key, class T>
+HashTable<Key,T>::~HashTable() {
+  //destructor - delete everything 
+}
+
+template <class Key, class T>
+unsigned long HashTable<Key,T>::calcIndex(Key k){
+	return hash(k) % backingArraySize;
+	//basic idea - >This indicates failure, since it is an impossible value
+	//find starting index. linear probing
+}
+
+template <class Key, class T>
+void HashTable<Key,T>::add(Key k, T x){
+  //check for numremoved- overwrite numitems+numremoved < backingarraysize
+	//check to see if need to grow
+
+	if((numRemoved + numItems)+1 > backingArraySize)
+	{ grow(); }
+
+	int start= calcIndex(k);
+	while(!backingArray[start].isDel && !backingArray[start].isNull)
+	{ start = (start+1) % backingArraySize; }
+	if(backingArray[start].isNull || backingArray[start].isDel)
+	{ 
+				if(backingArray[start].isNull)
+				{ numItems++; }
+				else
+				{numRemoved--; }
+
+				backingArray[start].k=k;
+				backingArray[start].x=x;
+				backingArray[start].isNull =false;
+				backingArray[start].isDel=false;
+	}
+	while(!backingArray[start].isDel && !backingArray[start].isNull)
+	{ start = (start+1) % backingArraySize; }
+
+}
+
+template <class Key, class T>
+void HashTable<Key,T>::remove(Key k){
+  //don't delete anything here
+ //flag things as isDel. so make isDel true
+	//3 possibilities - isNull, isDel
+}
+
+template <class Key, class T>
+T HashTable<Key,T>::find(Key k){
+  //TODO
+  T dummy;
+  return dummy;
+}
+
+template <class Key, class T>
+bool HashTable<Key,T>::keyExists(Key k){
+  //TODO
+  return false;
+}
+
+template <class Key, class T>
+unsigned long HashTable<Key,T>::size(){
+	return numItems;
+}
+
+template <class Key, class T>
+void HashTable<Key,T>::grow(){
+  //TODO check if numRemoved is greater than 0; 
+	//making new backing array- copying 
+	//delete old array
+	//numItems + numRemoved >= backingArraySize/2
+
+
+}
